562e4789874a2b68826f86a98c72510abae63680
juristr <juri.strumpflohner@gmail.com>
2016-05-17 14:08:35 +0200

    add routing support

--- src/app/craftyrol.component.ts
+++ src/app/craftyrol.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
+import { AdminComponent } from './+admin';
+import { Routes , ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from '@angular/router';
 
-import { PeopleListComponent } from './people-list/people-list.component';
+import { PeopleListComponent } from './+people/people-list/people-list.component';
 
 @Component({
   moduleId: module.id,
   selector: 'craftyrol-app',
-  directives: [ PeopleListComponent ],
+  directives: [ ROUTER_DIRECTIVES ],
   templateUrl: 'craftyrol.component.html',
-  styleUrls: ['craftyrol.component.css']
+  styleUrls: ['craftyrol.component.css'],
+  providers: [ROUTER_PROVIDERS]
 })
+@Routes([
+  {path: '/people', component: PeopleListComponent},
+  {path: '/admin', component: AdminComponent}
+])
 export class CraftyrolAppComponent {
   title = 'craftyrol works!';
 }
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import {HTTP_PROVIDERS} from '@angular/http';
+import { ROUTER_PROVIDERS } from '@angular/router';
 import { enableProdMode } from '@angular/core';
 import { CraftyrolAppComponent, environment } from './app/';
 
@@ -7,4 +8,7 @@ if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(CraftyrolAppComponent, [ HTTP_PROVIDERS ]);
+bootstrap(CraftyrolAppComponent, [
+  HTTP_PROVIDERS,
+  ROUTER_PROVIDERS
+  ]);
